a11441d8b3487e37559f7fa8841482dfa9c00956
9gig <tammynengi8@gmail.com>
2022-03-26 15:05:05 +0100

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# housingBackend
-# housingBackend
--- index.js
+++ index.js
@@ -1,35 +1,8 @@
-const express       = require('express');
-const mongoose      = require('mongoose');
+const express = require("express");
+const app = express();
 
-const unless        = require('express-unless');
-const cors          = require('cors');
-const path = require('path')
+app.get("/", function (req, res) {
+  res.send("WORKING!!!");
+});
 
-
-// GETTING AUTH & ERRORS MIDDLEWARES
-
-const bodyParser = require('body-parser');
-
-
-
-// INITIALIZING EXPRESS
-const app           = express();
-app.use(cors({origin:"*"}));
-
-
-
-  app.use(bodyParser.urlencoded({extended: true}));
-  // CHECKING TOKEN IF NEEDED
- 
-  app.get('/', (req, res)=>{
-    res.send(`<h1>It works</h1>`)
-  })
- 
-
-  // METHOD TO USE REQUEST AS AN OBJECT
-  app.use(express.json());
-
-  // MAIN LOCAL PORT CONECTION
-  app.listen(process.env.port || 3000, () =>{
-      console.log("Lets burst");
-  });
+app.listen(process.env.PORT || 5000);
