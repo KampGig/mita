51e8ded6e1043c4398b9358c6c68b79ca991f204
9gig <tammynengi8@gmail.com>
2022-05-22 16:54:38 +0100

    db stuff

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db:'mongodb://localhost:27017/kamper'
+    db:'mongodb+srv://CTO:!!neng!123@kampgig.cwnn2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 };
 
 //mongodb+srv://kamp:!!kampg!g4@kampgigdb.bm50c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority   
