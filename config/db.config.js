48c8ec3b32d52a506cc4c1711b766122258c2b55
9gig <tammynengi8@gmail.com>
2022-05-22 22:21:12 +0100

    db change

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    db:'mongodb+srv://CTO:!!neng!123@kampgig.cwnn2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    db:'mongodb+srv://kamp:!!kampg!g4@kampgigdb.bm50c.mongodb.net/housing?retryWrites=true&w=majority'
 };
 
 //mongodb+srv://kamp:!!kampg!g4@kampgigdb.bm50c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority   
 // mongodb+srv://<username>:<password>@kampgig.cwnn2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 // mongodb://localhost:27017/kamper
-
+// mongodb+srv://kamp:<password>@kampgigdb.bm50c.mongodb.net/?retryWrites=true&w=majority
 // db:'mongodb+srv://CTO:!!neng!123@kampgig.cwnn2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
